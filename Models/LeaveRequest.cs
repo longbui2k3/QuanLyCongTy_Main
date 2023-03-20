e072dd998f2d83f09084bad5351075bc0f40f003
jay <jay.anhui@orderprotection.com>
2023-03-20 20:34:59 +0700

    update chamcong

---
+++ Models/LeaveRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCongTy.Models
+{
+    public class LeaveRequest
+    {
+        public string LeaveRequestID { get; set; }
+        public string EmployeeID { get; set; }
+        public DateTime LeaveDate { get; set; }
+        public string Reason { get; set; }
+    }
+}
